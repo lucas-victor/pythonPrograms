143b7a1f7c511f2ed6c793ee180589efbcac5562
lucas.a.victor <lucas.a.victor@gmail.com>
2019-03-16 00:54:44 -0300

    inclusao arquivo.

---
+++ cursoPython/comandosLinuxSystem.py
@@ -0,0 +1,13 @@
+import os
+
+
+os.system("ls -lathr")
+
+os.system('echo "Foi exibido o comando ls -lathr..........................."')
+
+#guarda o exit code do comando. --0 sucesso  -- !=0 erro --por exemplo 1
+var = os.system('ls')
+print(var)
+
+#guarda a saída do comando executado no terminal
+var = os.pytho
